ec097bb4e461bef29e4f746bc150bd52ee71521c
moeenn <moeen.v8@gmail.com>
2021-05-06 14:40:51 +0500

    refinements

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Prevent "Last Seen" on Skype
 If you use Skype then you know that Skype tracks keypresses and mouse movements to detect the last time user was active. If a user doesn't move their mouse or press any keys for a while, Skype changes the user's status to "Recently active, Last seen n minutes ago". This is a short script that prevent this from happening.
 
+## How does it work?
+Skype detects user activity by frequency of keypresses and move movement. The script manages to fool skype by randomly pressing keys every n second (default 20 seconds).
+
 ## Installation
 ```bash
 $ pip3 install -r requirements.txt
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+"""
+  Skype detects user activity by frequency of keypresses and move movement. The
+  script manages to fool skype by randomly pressing keys every n second (default
+  20 seconds).
+"""
+
 from pynput.keyboard import Key, Controller
 import random
 import time
+import sys
 
 
+# generate a random number in provided range
 def generate_random(start, end):
 	random_num = random.randint(start, end + 1)
 	return random_num
 
 
+# press array of provided keys, n times
 def key_press(keyboard, keys, times=1):
-	for i in range(times):
+	for _ in range(times):
 		for key in keys:
 			keyboard.press(key)
 
@@ -17,22 +26,31 @@ def key_press(keyboard, keys, times=1):
 			keyboard.release(key)
 
 
+# main functionality of program
+def main():
+  keyboard = Controller()
+  delay = 20
+
+  while True:
+    tab_times = generate_random(10, 15)
+    switch_tab_times = generate_random(2, 6)
+
+    print(f"Sleeping {delay} seconds...")
+    time.sleep(delay)
+
+    print(f"Clicking {tab_times}th link")
+    key_press(keyboard, [Key.tab], tab_times)
+    key_press(keyboard, [Key.enter])
+
+    if (tab_times % 2 == 0):
+      print("Switching Tabs...")
+      key_press(keyboard, [Key.ctrl, Key.tab], switch_tab_times)
+
+
+# entry-point to the script
 if __name__ == "__main__":
-	keyboard = Controller()
-	delay = 10
-
-	while True:
-		tab_times = generate_random(15, 30)
-		switch_tab_times = generate_random(2, 6)
-
-		print(f"Sleeping {delay} seconds...")
-		time.sleep(delay)
-
-		key_press(keyboard, [Key.tab], tab_times)
-		print(f"Pressing Tab {tab_times} times")
-
-		key_press(keyboard, [Key.enter])
-
-		if (tab_times % 2 == 0):
-			print("Switching Tabs...")
-			key_press(keyboard, [Key.ctrl, Key.tab], switch_tab_times)
+  try:
+    main()
+  except KeyboardInterrupt:
+    print("Gracefully shutting down script...")
+    sys.exit(0)
